ce9efc6757160e0c545472017aa9bc8126f6cba9
2nthony <ant.hu@outlook.com>
2022-12-05 18:53:10 +0800

    feat: tab

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,19 +1,44 @@
-import { useState } from "react"
+import { useIsDark } from "recomposition-api"
+
+import { LogosTwitter } from "./icons/twitter"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const isDark = useIsDark()
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
+        width: 210,
+        padding: 8,
+        backgroundColor: isDark ? "rgb(52, 52, 52)" : "#fff",
+        color: isDark ? "#fff" : "#000"
       }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <h1 style={{ margin: 0 }}>Blank Tab Modern</h1>
+      <p>
+        The easiest way to make new tab as <b>blank tab</b> with no options.
+      </p>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end"
+        }}>
+        <a
+          href="https://twitter.com/_2nthony"
+          style={{
+            textDecoration: "none",
+            color: "#55acee",
+            display: "inline-flex",
+            alignItems: "center",
+            gap: 4
+          }}
+          target="_blank">
+          <LogosTwitter />
+          @2nthony
+        </a>
+      </div>
     </div>
   )
 }
